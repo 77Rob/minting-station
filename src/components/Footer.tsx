e627a780cfe91b64c5504d7003c7cd2e7bee6d3e
Your Name <you@example.com>
2023-03-11 05:07:18 +0100

    Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="mx-2 flex justify-end border-t-2 py-2 px-4 left-0 right-0 bottom-0 h-12 absolute mt-16 border-t-white ">
+      <div className="flex items-center gap-2">
+        <p className="text-xl font-bold">POWERED BY:</p>
+        <img src="/logo_mantle.svg" className="h-10" />
+      </div>
+    </footer>
+  );
+};
+export default Footer;
