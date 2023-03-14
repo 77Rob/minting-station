1b4beca4943aac61945a3e696b06a0d2ba96f550
777Rob <rcekanauskasnl@gmail.com>
2023-03-14 07:50:42 +0200

    Add crud to images

--- src/pages/create/images.tsx
+++ src/pages/create/images.tsx
@@ -1,7 +1,18 @@
 import FileUpload from "@/components/FileUpload";
 import { ContractSettings } from "../../components/ContractSettings";
 import { useAppDispatch, useAppSelector } from "@/store";
-import { IImage, Image, loadImages, uploadImages } from "@/store/imagesReducer";
+import {
+  IImage,
+  Image,
+  deleteImages,
+  deselectAllImages,
+  deselectImage,
+  loadImages,
+  selectAllImages,
+  selectImage,
+  updateMetadata,
+  uploadImages,
+} from "@/store/imagesReducer";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { Field, FieldInputProps, Form, Formik, FormikProps } from "formik";
@@ -13,13 +24,14 @@ import {
   FourColumnsIcon,
   MinusIcon,
 } from "@/assets";
+import { TrashIcon } from "@heroicons/react/24/solid";
 import Button from "@/components/Button";
 
 const Images = () => {
   const dispatch = useAppDispatch();
   const imagesState = useAppSelector((state) => state.images);
   useEffect(() => {
-    loadImages(dispatch);
+    loadImages({ dispatch });
   }, []);
 
   const [columns, setColumns] = useState(6);
@@ -27,17 +39,45 @@ const Images = () => {
   return (
     <div className="grid grid-cols-7 gap-8 px-8 mx-2 my-4">
       <div className="col-span-5 space-y-4">
-        <div className="card px-2 py-4 grid grid-cols-5">
+        <div className="card px-2 py-4 space-x-8 items-center grid grid-cols-5">
           <FileUpload
             className="col-span-2"
             onFiles={(files) => {
-              uploadImages(files, dispatch);
+              uploadImages({ images: files, dispatch });
             }}
           />
-          <div className="col-span-2" />
           <div className="col-span-1 flex flex-col gap-2">
-            <button className="btn-red">Reset</button>
-            <button className="btn-primary">Load Demo</button>
+            <Button className="btn-red btn-sm">Reset</Button>
+            <Button className="btn-primary btn-sm">Load Demo</Button>
+          </div>
+          <div className="space-y-2 items-end flex flex-col col-span-2">
+            <Button
+              className="w-[65%] btn-red px-3 flex text-center
+             items-center btn-sm"
+              onClick={() =>
+                deleteImages({
+                  fileNames: imagesState.selected,
+                  dispatch,
+                })
+              }
+            >
+              <p className="flex flex-grow justify-center ml-6">
+                DELETE SELECTED
+              </p>
+              <TrashIcon className="w-6 justify-self-end" />
+            </Button>
+            <Button
+              className="btn-primary w-[65%]  btn-sm "
+              onClick={() => dispatch(selectAllImages())}
+            >
+              SELECT ALL
+            </Button>
+            <Button
+              className="btn-primary btn-sm  w-[65%] "
+              onClick={() => dispatch(deselectAllImages())}
+            >
+              DESELECT ALL
+            </Button>
           </div>
         </div>
         <div className="card py-2  w-full overflow-auto h-screen">
@@ -56,7 +96,7 @@ const Images = () => {
           >
             {imagesState.images &&
               imagesState.images?.map((image, index) => (
-                <NFTImage key={image.id} {...image} columns={columns} />
+                <NFTImage key={image.fileName} {...image} columns={columns} />
               ))}
           </div>
         </div>
@@ -73,8 +113,46 @@ type INFTImage = IImage & {
   columns: number;
 };
 
+const NFTHeader = ({
+  name,
+  fileName,
+  setFlipped,
+}: {
+  name: string;
+  fileName: string;
+  setFlipped: (flipped: any) => void;
+}) => {
+  const dispatch = useAppDispatch();
+  const selected = useAppSelector((state) => state.images.selected);
+
+  return (
+    <div className="flex justify-between items-center px-2 py-1">
+      <input
+        className="bg-inherit w-6 h-6 border-white border-2 rounded-md ring-0 checked:text-white checked:bg-gray-100 checkbox-fix focus:ring-0 cursor-pointer"
+        type="checkbox"
+        checked={selected.includes(fileName)}
+        onChange={(e) => {
+          if (e.target.checked) {
+            dispatch(selectImage(fileName));
+          } else {
+            dispatch(deselectImage(fileName));
+          }
+        }}
+      />
+      <h1 className="text-xl font-bold">{name}</h1>
+      <motion.button
+        whileTap={{ scale: 0.92 }}
+        onClick={() => setFlipped((flipped: boolean) => !flipped)}
+        whileHover={{ scale: 1.08 }}
+      >
+        <GearIcon />
+      </motion.button>
+    </div>
+  );
+};
+
 const NFTImage = ({
-  id,
+  fileName,
   name,
   url,
   attributes,
@@ -83,31 +161,14 @@ const NFTImage = ({
 }: INFTImage) => {
   const [flipped, setFlipped] = useState(false);
 
-  const NFTHeader = () => {
-    return (
-      <div className="flex justify-between items-center px-2 py-1">
-        <input
-          className="bg-inherit w-6 h-6 border-white border-2 rounded-md ring-0 checked:text-white checked:bg-gray-100 checkbox-fix focus:ring-0 cursor-pointer"
-          type="checkbox"
-        />
-        <h1 className="text-xl font-bold">{name}</h1>
-        <motion.button
-          whileTap={{ scale: 0.92 }}
-          onClick={() => setFlipped((flipped) => !flipped)}
-          whileHover={{ scale: 1.08 }}
-        >
-          <GearIcon />
-        </motion.button>
-      </div>
-    );
-  };
-
   // Styles object so changes affect both card and settings
   const styles = {
     card: `border-[3px] bg-base-100 border-highlight rounded-xl flex flex-col  ${
       columns !== 1 && "h-80 max-h-80"
     }`,
   };
+
+  const dispatch = useAppDispatch();
 
   const NFTSettingsCard = () => {
     return (
@@ -122,7 +183,7 @@ const NFTImage = ({
         }}
         className={styles.card + "p-2 overflow-y-auto "}
       >
-        <NFTHeader />
+        <NFTHeader name={name} setFlipped={setFlipped} fileName={fileName} />
         <div className="flex flex-col rounded-xl justify-items-center p-2">
           <Formik
             initialValues={{
@@ -131,7 +192,16 @@ const NFTImage = ({
               attributes: attributes || [],
             }}
             onSubmit={(values) => {
-              console.log(values);
+              updateMetadata({
+                imageData: {
+                  fileName,
+                  name: values.name,
+                  url: url,
+                  description: values.description,
+                  attributes: values.attributes,
+                },
+                dispatch,
+              });
             }}
           >
             <Form className={`${columns == 1 ? "space-y-1" : "space-y-4"} `}>
@@ -161,7 +231,7 @@ const NFTImage = ({
               </div>
 
               <Field name="attributes" component={AttributesField} />
-              <Button type="submit" className="w-full mt-2">
+              <Button type="submit" className="btn-primary w-full mt-2">
                 Save
               </Button>
             </Form>
@@ -180,12 +250,11 @@ const NFTImage = ({
         whileInView={{
           transition: { duration: 0.3, ease: "easeInOut" },
           opacity: 1,
-
           rotateY: [180, 140, 80, 50, 20],
         }}
         className={styles.card}
       >
-        <NFTHeader />
+        <NFTHeader name={name} setFlipped={setFlipped} fileName={fileName} />
         <div className="flex items-center  justify-center  border-[3px] h-full border-highlight ">
           <img
             className={
