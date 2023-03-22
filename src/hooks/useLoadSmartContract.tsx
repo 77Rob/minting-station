bd56f2e512c8642bbb8d04a01508568a097f0844
777Rob <126085955+77Rob@users.noreply.github.com>
2023-03-22 19:00:50 +0200

    Create useLoadSmartContract hook

--- src/hooks/useLoadSmartContract.tsx
+++ src/hooks/useLoadSmartContract.tsx
@@ -1,56 +1,90 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
 import axios from "axios";
-import { useSigner, useProvider, useContract } from "wagmi";
-import { Contract, Signer } from "ethers";
+import { BigNumber, Contract } from "ethers";
+import { formatUnits } from "ethers/lib/utils.js";
+import { useEffect, useState } from "react";
+import { useProvider } from "wagmi";
+
+export interface ContractData {
+  name: string;
+  description: string;
+  image: string;
+  external_url: string;
+  attributes: any[];
+  owner: string;
+  totalSupply: number;
+  maxSupply: number;
+  price: number;
+  saleIsActive: boolean;
+}
 
 export default function useLoadCollection({
   contractAddress,
 }: {
   contractAddress: string | undefined | string[];
 }) {
-  const [contractData, setContractData] = useState<any>(undefined);
-  const [loading, setLoading] = useState<boolean>(false);
+  const [data, setData] = useState<any>(undefined);
+  const [loading, setLoading] = useState<boolean>(true);
   const [contractExists, setContractExists] = useState<boolean | undefined>(
     undefined
   );
-  const signer = useSigner();
+  const [abi, setABI] = useState<any>([]);
+  const provider = useProvider();
+  const [contract, setContract] = useState<any>(undefined);
+
+  const loadContract = async (ethersContract: any) => {
+    const loaded = {
+      saleIsActive: Boolean(await ethersContract.saleIsActive()),
+      maxSupply: (await ethersContract.MAX_SUPPLY()).toString(),
+      owner: await ethersContract.owner(),
+      totalSupply:
+        (await ethersContract.totalSupply()).toString() || "unlimited",
+      price: formatUnits(
+        BigNumber.from(
+          (
+            (ethersContract.PRICE && (await ethersContract.PRICE())) ||
+            "0"
+          ).toString()
+        ),
+        18
+      ),
+    };
+    console.log(loaded);
+    return loaded;
+  };
 
   useEffect(() => {
     const loadContractData = async () => {
       if (typeof contractAddress == "string") {
         const contractDataRequest = await axios.get(
           `${process.env.NEXT_PUBLIC_API_URL}/collection/${contractAddress}`,
-          {
-            params: {
-              contractAddress: contractAddress,
-            },
-          }
+          {}
         );
         if (contractDataRequest.data === undefined) {
           setContractExists(false);
-        } else {
-          setContractExists(true);
+          return;
         }
-        setContractData(contractDataRequest.data);
+
+        let contractData = contractDataRequest.data;
+        const contractAbi = JSON.parse(contractData?.abi || "[]");
+        const ethersContract = new Contract(
+          contractAddress,
+          contractAbi,
+          provider
+        );
+        setContract(ethersContract);
+        setData({ ...contractData, ...(await loadContract(ethersContract)) });
+        setContractExists(true);
+        setContract(contract);
+        setABI(contractAbi);
         setLoading(false);
       }
     };
     loadContractData();
   }, [contractAddress]);
 
-  const contract = useMemo(() => {
-    if (!loading && contractExists) {
-      return new Contract(
-        contractAddress as string,
-        contractData.abi,
-        signer as unknown as Signer
-      );
-    }
-  }, [contractData]);
-
   return {
     contract,
-    contractData,
+    data,
     loading,
     contractExists,
   };
