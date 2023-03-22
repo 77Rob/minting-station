61476553144ee2b768dd9a429cd4072802ba90fd
777Rob <126085955+77Rob@users.noreply.github.com>
2023-03-22 19:00:37 +0200

    fix

--- src/components/MintNFTButton.tsx
+++ src/components/MintNFTButton.tsx
@@ -12,35 +12,13 @@ const MintNFTButton = ({ contractAddress }: Props) => {
     setIsLoading(true);
 
     try {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const provider = new ethers.providers.Web3Provider(
+        window.ethereum as any
+      );
       const signer = provider.getSigner();
       const contract = new ethers.Contract(
         contractAddress,
         ["function mint() public payable returns (uint256)"],
-        signer
-      );
-
-      const tx = await contract.mint();
-
-      await tx.wait();
-
-      alert("NFT minted successfully!");
-    } catch (error) {
-      console.error(error);
-      alert("Failed to mint NFT");
-    }
-
-    setIsLoading(false);
-  };
-  const handleCheckBalance = async () => {
-    setIsLoading(true);
-
-    try {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const contract = new ethers.Contract(
-        contractAddress,
-        ["function balanceOf() public returns (uint256)"],
         signer
       );
 
